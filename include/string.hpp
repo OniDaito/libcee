e77440da6514b928a1b6314e799e514cc629491a
Benjamin Blundell <me@benjamin.computer>
2022-04-25 12:40:12 +0100

    Libcee installer script is sorted now

--- include/string.hpp
+++ include/string.hpp
@@ -303,13 +303,15 @@ inline char *itob(long x) {
 */
 // trim from start
 
-static inline std::string &ltrim(std::string &s) {
+static inline std::string &ltrim(std::string const &in_str) {
+  std::string s = in_str;
   s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
   return s;
 }
 
 // trim from end
-static inline std::string &rtrim(std::string &s) {
+static inline std::string &rtrim(std::string const &in_str) {
+  std::string s = in_str;
   s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
   return s;
 }
