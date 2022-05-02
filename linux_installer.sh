4c48694f13aa81ac48e2db8b5881c2088f8da8ac
Benjamin Blundell <me@benjamin.computer>
2022-05-02 12:34:48 +0100

    Better installer

--- linux_installer.sh
+++ linux_installer.sh
@@ -9,6 +9,6 @@ mkdir -p $libdir
 mkdir -p $incdir
 
 # Copy the main library and the headers 
-cp "libcee.so" "${libdir}/."
-cp -r "../include/." "${incdir}/."
-cp "../libcee.pc" "${pkgdir}/."
+cp "${MESON_BUILD_ROOT}/libcee.so" "${libdir}/."
+cp -r "${MESON_SOURCE_ROOT}/include/." "${incdir}/."
+cp "${MESON_SOURCE_ROOT}/libcee.pc" "${pkgdir}/."
