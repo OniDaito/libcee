2e788fd0204e1d08b87f626b976cd0edc7848c5d
Benjamin Blundell <me@benjamin.computer>
2022-10-06 14:24:31 +0100

    Added a linear mix function

--- include/math.hpp
+++ include/math.hpp
@@ -2,10 +2,14 @@
 #define __libcee_MATH_H__
 
 /**
- *  _____   _______ ______ _______ _______ _______ 
- * |     |_|_     _|   __ \     __|    ___|    ___|
- * |       |_|   |_|   __ <__     |    ___|    ___|
- * |_______|_______|______/_______|_______|_______|
+ *  (     (                           
+ *  )\ )  )\ )   (     (              
+ * (()/( (()/( ( )\    )\   (    (    
+ *  /(_)) /(_)))((_) (((_)  )\   )\   
+ * (_))  (_)) ((_)_  )\___ ((_) ((_)  
+ * | |   |_ _| | _ )((/ __|| __|| __| 
+ * | |__  | |  | _ \ | (__ | _| | _|  
+ * |____||___| |___/  \___||___||___| 
  *                                               
  * @file util_math.h
  * @author Benjamin Blundell - me@benjamin.computer
@@ -13,6 +17,9 @@
  * @brief Basic maths utilities.
  *
  */
+
+#include <iostream>
+#include "macros.hpp"
 
 namespace libcee {
 static const double PI = M_PI;
@@ -22,6 +29,21 @@ inline static double DegToRad(const double &x) { return x * PI / 180.0; }
 
 inline static float RadToDeg(const float &x) { return x * 180.0 / PI; }
 inline static float DegToRad(const float &x) { return x * PI / 180.0; }
+
+/**
+ * @brief Mix linearly between two values, given a value between 0 and 1
+ * 
+ * @param x 
+ * @param y 
+ * @param v 
+ * @return float 
+ */
+inline static float Mix(const float &x, const float &y, const float &v) {
+    if (v < 0.0) { return x;}
+    else if (v > 1.0) { return y;}
+    return x * (1.0 - v) + y * v;
+}
+
 }
 
 #endif
